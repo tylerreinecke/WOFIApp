ae02b9eea6b82a55c1f8974f337265cb469ea2a9
ThomasTwomey <league@WTS-iMac-11.local>
2017-07-09 18:33:47 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/league/wofiapp/WOFIApp.java
@@ -0,0 +1,13 @@
+package com.example.league.wofiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WOFIApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wofiapp);
+    }
+}
